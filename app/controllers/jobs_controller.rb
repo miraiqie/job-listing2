ec486e289f7cf26362fba570f96eb02c3aa4315a
mirai <miraiqie@gmail.com>
2018-11-11 22:00:01 +0800

    implement step2

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,55 @@
+class JobsController < ApplicationController
+
+  def index
+    @jobs = Job.all
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      flash[:notice] = "Add a job success!"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      flash[:notice] = "Edit Success!"
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    if @job.destroy
+      flash[:notice] = "Delete Success!"
+      redirect_to root_path
+    end
+  end
+
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181111115548) do
+ActiveRecord::Schema.define(version: 20181111124331) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
