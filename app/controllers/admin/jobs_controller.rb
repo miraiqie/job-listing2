595a577856b81440c89befbe1c433b96cdefda31
mirai <miraiqie@gmail.com>
2018-11-12 23:57:42 +0800

    implement wage  part

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -52,7 +52,7 @@ private
 
 
 def job_params
-  params.require(:job).permit(:title, :description)
+  params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email)
 end
 
 end
